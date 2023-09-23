e16b3c95edb07b69a56cd293ff6fc62da22c789e
Mikita-Arsi <ardonnik@gmail.com>
2023-09-23 19:21:53 +0300

    fix tests

--- test/lab0_test.cpp
+++ test/lab0_test.cpp
@@ -17,15 +17,9 @@ TEST(NegativeErrTest_a, BasicAsExasertions) {
    int a{-3}, b{7}, res;
 
    // act
-   try {
-      res = count_of_non_zero_bits_in_range(a, b);
-   } catch (std::invalid_argument const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Invalid input, number must be unsignused"));
-   } catch (...) {
-      FAIL() << "Expected std::invalid_argument";
-   }
+
+   // assert
+   EXPECT_THROW(count_of_non_zero_bits_in_range(a, b), std::invalid_argument);
 }
 
 TEST(NegativeErrTest_b, BasicAsExasertions) {
@@ -33,15 +27,9 @@ TEST(NegativeErrTest_b, BasicAsExasertions) {
    int a{2}, b{-7}, res;
 
    // act
-   try {
-      res = count_of_non_zero_bits_in_range(a, b);
-   } catch (std::invalid_argument const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Invalid input, number must be unsignused"));
-   } catch (...) {
-      FAIL() << "Expected std::invalid_argument";
-   }
+
+   // assert
+   EXPECT_THROW(count_of_non_zero_bits_in_range(a, b), std::invalid_argument);
 }
 
 TEST(CompareErrTest, BasicAsExasertions) {
@@ -49,92 +37,190 @@ TEST(CompareErrTest, BasicAsExasertions) {
    int a{8}, b{7}, res;
 
    // act
-   try {
-      res = count_of_non_zero_bits_in_range(a, b);
-   } catch (std::out_of_range const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Number \"b\" must be more than number \"a\""));
-   } catch (...) {
-      FAIL() << "Expected std::out_of_range";
-   }
+
+   // assert
+   EXPECT_THROW(count_of_non_zero_bits_in_range(a, b), std::out_of_range);
 }
 TEST(Func__validate_nums__NegativeErrTest_a, BasicAsExasertions) {
    // arrange
    int a{-8}, b(7);
+
    // act
-   try {
-      validate_nums(a, b);
-   } catch (std::invalid_argument const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Invalid input, number must be unsignused"));
-   } catch (...) {
-      FAIL() << "Expected std::invalid_argument";
-   }
+
+   // assert
+   EXPECT_THROW(validate_nums(a, b), std::invalid_argument);
 }
 
 TEST(Func__validate_nums__NegativeErrTest_b, BasicAsExasertions) {
    // arrange
    int a{2}, b(-7);
+
    // act
-   try {
-      validate_nums(a, b);
-   } catch (std::invalid_argument const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Invalid input, number must be unsignused"));
-   } catch (...) {
-      FAIL() << "Expected std::invalid_argument";
-   }
+
+   // assert
+   EXPECT_THROW(validate_nums(a, b), std::invalid_argument);
 }
 
 TEST(Func__validate_nums__CompareErrTest, BasicAsExasertions) {
    // arrange
    int a{8}, b(7);
-   // act
-   try {
-      validate_nums(a, b);
-   } catch (std::out_of_range const& err) {
-      // assert
-      EXPECT_EQ(err.what(),
-                std::string("Number \"b\" must be more than number \"a\""));
-   } catch (...) {
-      FAIL() << "Expected std::out_of_range";
-   }
-}
-
-TEST(Func__pow2__Test, BasicAsExasertions) {
-   // arrange
 
    // act
 
    // assert
-   EXPECT_EQ(pow2(0), 1);
-   EXPECT_EQ(pow2(1), 2);
-   EXPECT_EQ(pow2(2), 4);
-   EXPECT_EQ(pow2(3), 8);
+   EXPECT_THROW(validate_nums(a, b), std::out_of_range);
 }
 
-TEST(Func__not_equal_reminder__Test, BasicAsExasertions) {
+TEST(Func__pow2__Test1, BasicAsExasertions) {
    // arrange
+   int power{0}, res;
 
    // act
+   res = pow2(power);
 
    // assert
-   EXPECT_EQ(not_equal_reminder(11, 1), true);
-   EXPECT_EQ(not_equal_reminder(11, 2), false);
-   EXPECT_EQ(not_equal_reminder(8, 2), false);
-   EXPECT_EQ(not_equal_reminder(8, 3), true);
+   EXPECT_EQ(res, 1);
 }
 
-TEST(Func__calculating_the_range_difference__Test, BasicAsExasertions) {
+TEST(Func__pow2__Test2, BasicAsExasertions) {
    // arrange
+   int power{1}, res;
 
    // act
+   res = pow2(power);
 
    // assert
-   EXPECT_EQ(calculating_the_range_difference(2, 8), 11);
-   EXPECT_EQ(calculating_the_range_difference(0, 2), 1);
-   EXPECT_EQ(calculating_the_range_difference(10, 12), 5);
+   EXPECT_EQ(res, 2);
+}
+
+TEST(Func__pow2__Test3, BasicAsExasertions) {
+   // arrange
+   int power{2}, res;
+
+   // act
+   res = pow2(power);
+
+   // assert
+   EXPECT_EQ(res, 4);
+}
+
+TEST(Func__pow2__Test4, BasicAsExasertions) {
+   // arrange
+   int power{3}, res;
+
+   // act
+   res = pow2(power);
+
+   // assert
+   EXPECT_EQ(res, 8);
+}
+
+TEST(Func__not_equal_reminder__Test1, BasicAsExasertions) {
+   // arrange
+   int num{11}, power{1}, res;
+
+   // act
+   res = not_equal_reminder(num, power);
+
+   // assert
+   EXPECT_EQ(res, true);
+}
+
+TEST(Func__not_equal_reminder__Test2, BasicAsExasertions) {
+   // arrange
+   int num{11}, power{2}, res;
+
+   // act
+   res = not_equal_reminder(num, power);
+
+   // assert
+   EXPECT_EQ(res, false);
+}
+
+TEST(Func__not_equal_reminder__Test3, BasicAsExasertions) {
+   // arrange
+   int num{8}, power{2}, res;
+
+   // act
+   res = not_equal_reminder(num, power);
+
+   // assert
+   EXPECT_EQ(res, false);
+}
+
+TEST(Func__not_equal_reminder__Test4, BasicAsExasertions) {
+   // arrange
+   int num{8}, power{3}, res;
+
+   // act
+   res = not_equal_reminder(num, power);
+
+   // assert
+   EXPECT_EQ(res, true);
+}
+
+TEST(Func__calculating_the_range_difference__Test1, BasicAsExasertions) {
+   // arrange
+   int a{2}, b{8}, res;
+
+   // act
+   res = calculating_the_range_difference(a, b);
+
+   // assert
+   EXPECT_EQ(res, 11);
+}
+
+TEST(Func__calculating_the_range_difference__Test2, BasicAsExasertions) {
+   // arrange
+   int a{0}, b{2}, res;
+
+   // act
+   res = calculating_the_range_difference(a, b);
+
+   // assert
+   EXPECT_EQ(res, 1);
+}
+
+TEST(Func__calculating_the_range_difference__Test3, BasicAsExasertions) {
+   // arrange
+   int a{10}, b{12}, res;
+
+   // act
+   res = calculating_the_range_difference(a, b);
+
+   // assert
+   EXPECT_EQ(res, 5);
+}
+
+TEST(Func__add_iteration__Test1, BasicAsExasertions) {
+   // arrange
+   int num{6}, power{1}, res;
+
+   // act
+   res = add_iteration(num, power);
+
+   // assert
+   EXPECT_EQ(res, 3);
+}
+
+TEST(Func__add_iteration__Test2, BasicAsExasertions) {
+   // arrange
+   int num{6}, power{2}, res;
+
+   // act
+   res = add_iteration(num, power);
+
+   // assert
+   EXPECT_EQ(res, 4);
+}
+
+TEST(Func__add_iteration__Test3, BasicAsExasertions) {
+   // arrange
+   int num{9}, power{3}, res;
+
+   // act
+   res = add_iteration(num, power);
+
+   // assert
+   EXPECT_EQ(res, 5);
 }
