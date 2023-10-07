f7e35862dd149ca4698b160fddea41ffb25be3d2
Mikita-Arsi <ardonnik@gmail.com>
2023-10-07 17:01:45 +0300

    fix tests

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
 #include <iostream>
 #include "Decimal.h"
 
-void print_array(unsigned char* arr, int n) {
-   for (int i = n - 1; i > -1; i -= 1) {
-      std::cout << arr[i];
-   }
-   std::cout << std::endl;
-}
-
-int main() {
-   Decimal c("222"), a("23");
-   Decimal& a_a = a;
-   Decimal b(a_a);
-   print_array(a.get_array(), a.get_size());
-   print_array(b.get_array(), b.get_size());
-   print_array(c.get_array(), c.get_size());
-   return 0;
-}
+int main() { return 0; }
