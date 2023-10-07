4f5c51d8ae431800b599f2ddafd5a37123d1ebae
Mikita-Arsi <ardonnik@gmail.com>
2023-10-07 14:25:43 +0300

    lab1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "Decimal.h"
 
-#include "lib.h"
-
-using namespace std;
+void print_array(unsigned char* arr, int n) {
+   for (int i = n - 1; i > -1; i -= 1) {
+      std::cout << arr[i];
+   }
+   std::cout << std::endl;
+}
 
 int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
+   Decimal c("222"), a("23");
+   Decimal& a_a = a;
+   Decimal b(a_a);
+   print_array(a.get_array(), a.get_size());
+   print_array(b.get_array(), b.get_size());
+   print_array(c.get_array(), c.get_size());
+   return 0;
 }
