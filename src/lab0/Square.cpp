5affb868ec242e84cca34e6f458d08ffc21da073
Mikita-Arsi <ardonnik@gmail.com>
2024-02-17 15:54:06 +0300

    init

---
+++ src/lab0/Square.cpp
@@ -0,0 +1,8 @@
+#include "Square.hpp"
+
+Square::Square() = default;
+
+Square::Square(const Point &a, const Point &b, const Point &c, const Point &d) : Rectangle(a, b, c, d)
+{
+    // Validator::validateSquare(a, b, c, d);
+}
